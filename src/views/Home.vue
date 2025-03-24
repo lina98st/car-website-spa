adf60033dbbabc7f9845ea7dc897d7b1067dc2fd
lina98st <alina29st@gmail.com>
2025-03-24 13:02:20 +0100

    jpg to webp changed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,9 +11,9 @@ interface Car {
 
 // Typisierte Liste von Autos
 const cars: Car[] = [
-  { id: 1, name: 'BMW M3', price: 65000, img: './images/bmw-m3.jpg' },
-  { id: 2, name: 'Audi RS5', price: 70000, img: './images/audi-rs5.jpg' },
-  { id: 3, name: 'Mercedes AMG C63', price: 85000, img: './images/mercedes-amg-c63.jpg' },
+  { id: 1, name: 'BMW M3', price: 65000, img: './images/bmw-m3.webp' },
+  { id: 2, name: 'Audi RS5', price: 70000, img: './images/audi-rs5.webp' },
+  { id: 3, name: 'Mercedes AMG C63', price: 85000, img: './images/mercedes-amg-c63.webp' },
 ]
 </script>
 
@@ -21,10 +21,11 @@ const cars: Car[] = [
   <div class="bg-gradient-to-br from-gray-900 via-gray-800 to-black text-black min-h-screen py-16">
     <div class="container mx-auto px-8">
       <h1
-        class="text-6xl font-extrabold text-center text-yellow-400 mb-10 tracking-wide drop-shadow-xl"
+        class="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold text-center text-yellow-400 mb-10 tracking-wide drop-shadow-xl"
       >
         Luxus & Performance
       </h1>
+
       <p class="text-lg text-gray-300 text-center max-w-3xl mx-auto mb-16 leading-relaxed">
         Erlebe eine exklusive Auswahl an Premium-Fahrzeugen – von sportlichen Limousinen bis hin zu
         luxuriösen SUVs.
